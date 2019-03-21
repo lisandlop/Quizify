ba80bbf579a1cf3c53bdcf3e41b9af43b2d3f1fd
jura <jura@kth.se>
2019-03-21 11:15:14 +0100

    Select quiz created

--- src/containers/SelectPage.js
+++ src/containers/SelectPage.js
@@ -4,23 +4,13 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import '../styles/SelectPage.scss';
 
-import Quizzes from '../components/Quizzes/Quizzes'
+import SelectQuiz from '../components/SelectQuiz/SelectQuiz'
 
 class SelectPage extends Component {
   render() {
     return (
       <div className="SelectPage">
-        <Container fluid={true}>
-          <Row>
-            <Col xs={6}>
-              <Quizzes/>
-            </Col>
-
-            <Col xs={6}>
-              <h1>Select Quiz</h1>
-            </Col>
-          </Row>
-        </Container>
+        <SelectQuiz> </SelectQuiz>
       </div>
     );
   }
