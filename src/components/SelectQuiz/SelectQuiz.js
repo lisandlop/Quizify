7f033254249da80ca4de87b636d4c22751207fc6
thelmasv <thelmasv@gmail.com>
2019-03-28 15:20:56 +0100

    confirmbutton

--- src/components/SelectQuiz/SelectQuiz.js
+++ src/components/SelectQuiz/SelectQuiz.js
@@ -7,6 +7,8 @@ import Form from 'react-bootstrap/Form';
 import Table from 'react-bootstrap/Table';
 import { withFirebase } from '../Firebase';
 import Button from 'react-bootstrap/Button';
+import './SelectQuiz.scss'
+
 
 
 
@@ -44,11 +46,16 @@ class SelectQuiz extends Component {
   <Form.Group controlId="exampleForm.Quiz">
     <Form.Label>Search quiz name</Form.Label>
     <input type="text" className="form-control mr-sm-3" placeholder={"Enter song"}/>
+
   </Form.Group>
 
   <Form.Group controlId="exampleForm.Author">
     <Form.Label>Search author of quiz</Form.Label>
     <Form.Control type="text" placeholder="Q_master" />
+  
+    <Col sm={6}>
+    <th>Quiz Name</th>
+    </Col>
   </Form.Group>
   
   <Form.Group controlId="exampleForm.TypeOfMusic">
@@ -78,7 +85,19 @@ class SelectQuiz extends Component {
   </Table>
           </Col>
         </Row>
+    <Row md={12}>
+      <Col md={3}>
+        <div className = "Confirm" >
+          <Button variant="primary" size="lg" className="ConfirmButton" onClick={() => this.props.SelectQuiz(true)}>
+            <span>Confirm</span>
+          </Button>
+        </div>
+      </Col>
+    </Row>
         </Container></div>
+
+
+
 
       ); 
     }
