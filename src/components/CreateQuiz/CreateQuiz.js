b4570a8991de9001e80fd36332ab2b71c006c13d
Jonatan Lindström <jonatan3@kth.se>
2019-04-07 20:56:53 +0200

    Cleared warnings

--- src/components/CreateQuiz/CreateQuiz.js
+++ src/components/CreateQuiz/CreateQuiz.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { compose, renderComponent } from 'recompose';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,77 +7,75 @@ import Table from 'react-bootstrap/Table';
 import { withFirebase } from '../Firebase';
 import Button from 'react-bootstrap/Button';
 import './CreateQuiz.scss';
-//import './CreateQuestion.js';
 
 
 class CreateQuiz extends Component {
-    constructor(props) {
-      super(props);
-      
-    }
-      
-render() {
-    
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+
     return (
-        <div>
-          <p id="selectaquiz">Create a Quiz</p>
-    
-  <Container>
-    <Row>
-    <Col sm={6}>
-    <Form>
+      <div>
+        <p id="selectaquiz">Create a Quiz</p>
 
-<Form.Group controlId="CreateForm.Author">
-    <Form.Label>Enter author name</Form.Label>
-    <input type="text" className="form-control mr-sm-3" placeholder={"Author name"}/>
-</Form.Group>
+        <Container>
+          <Row>
+            <Col sm={6}>
+              <Form>
 
-<Form.Group controlId="CreateForm.QuizName">
-    <Form.Label>Enter quiz name</Form.Label>
-    <Form.Control type="text" placeholder="Quiz name" />
-</Form.Group>
-  
+                <Form.Group controlId="CreateForm.Author">
+                  <Form.Label>Enter author name</Form.Label>
+                  <input type="text" className="form-control mr-sm-3" placeholder={"Author name"} />
+                </Form.Group>
 
-{/* LÄGGA TILL EN KNAPP SOM LÄGGER TILL FRÅGA */}
-
-<Button variant="primary" size="lg" className="AddButton" onClick={() => this.props.CreateQuestion}>
-    <span>Add question</span>
-</Button>
-
-</Form>
-          </Col>
-          <Col md="auto">
-          <Table striped bordered hover>
-          
-
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Quiz Name</th>
-      <th>Author Name</th>
-      <th>Genre</th>
-    </tr>
-    {/* <tr>{allQuizzes}</tr> */}
-  </thead>
+                <Form.Group controlId="CreateForm.QuizName">
+                  <Form.Label>Enter quiz name</Form.Label>
+                  <Form.Control type="text" placeholder="Quiz name" />
+                </Form.Group>
 
 
-  </Table>
-          </Col>
-        </Row>
-    <Row md={12}>
-      <Col md={3}>
-        <div className = "Confirm" >
-          <Button variant="primary" size="lg" className="ConfirmButton" onClick={() => this.props.SelectQuiz(true)}>
-            <span>Confirm</span>
-          </Button>
-        </div>
-      </Col>
-    </Row>
+                {/* LÄGGA TILL EN KNAPP SOM LÄGGER TILL FRÅGA */}
+
+                <Button variant="primary" size="lg" className="AddButton" onClick={() => this.props.CreateQuestion}>
+                  <span>Add question</span>
+                </Button>
+
+              </Form>
+            </Col>
+            <Col md="auto">
+              <Table striped bordered hover>
+
+
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Quiz Name</th>
+                    <th>Author Name</th>
+                    <th>Genre</th>
+                  </tr>
+                  {/* <tr>{allQuizzes}</tr> */}
+                </thead>
+
+
+              </Table>
+            </Col>
+          </Row>
+          <Row md={12}>
+            <Col md={3}>
+              <div className="Confirm" >
+                <Button variant="primary" size="lg" className="ConfirmButton" onClick={() => this.props.SelectQuiz(true)}>
+                  <span>Confirm</span>
+                </Button>
+              </div>
+            </Col>
+          </Row>
         </Container></div>
 
-      ); 
-    }
+    );
   }
-  export default withFirebase(CreateQuiz);
+}
+export default withFirebase(CreateQuiz);
 
-   
--- src/components/StartQuiz/StartQuiz.js
+++ src/components/StartQuiz/StartQuiz.js
@@ -3,10 +3,6 @@ import Button from 'react-bootstrap/Button';
 import './StartQuiz.scss'
 
 class QuizStart extends Component {
-  constructor(props) {
-      super(props);
-  }
-
   render() {
     return (
       <div>
